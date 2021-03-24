b3aae575c2c8a0d9d1497ab9451615f40be0db66
totinIstvan <totin.istvan@gmail.com>
2021-03-24 16:59:58 +0100

    Refactor solution

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -26,40 +26,36 @@ public class RadioCharts {
     }
 
     public String getMostPlayedSong() {
-        String song = "";
+        String requestedColumn = "song";
         String SQL = "SELECT song, SUM(times_aired) as times_aired " +
                 "FROM music_broadcast " +
                 "GROUP BY artist " +
                 "ORDER BY times_aired DESC " +
                 "LIMIT 1";
-        try (Connection conn = this.getConnection()) {
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(SQL);
-            if (rs.next()) {
-                song = rs.getString("song");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return song;
+        return getResultOfQuery(SQL, requestedColumn);
     }
 
     public String getMostActiveArtist() {
-        String artist = "";
+        String requestedColumn = "artist";
         String SQL = "SELECT artist, COUNT(DISTINCT song) AS song " +
                 "FROM music_broadcast " +
                 "GROUP BY artist " +
                 "ORDER BY song " +
                 "DESC LIMIT 1";
+        return getResultOfQuery(SQL, requestedColumn);
+    }
+
+    private String getResultOfQuery(String SQL, String column) {
+        String res = "";
         try (Connection conn = this.getConnection()) {
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery(SQL);
             if (rs.next()) {
-                artist = rs.getString("artist");
+                res = rs.getString(column);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return artist;
+        return res;
     }
 }
